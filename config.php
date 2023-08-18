51db8d33d63fb1370175cd5aef178b15287fe513
Denis progman <95615037+denis-progman@users.noreply.github.com>
2023-08-17 23:43:38 -0400

    fix conf

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 const DATE_OF_PROJECT_START = "10/02/2023";
 const MIN_USER_COUNT = 123;
-const AWS_S3_BACKED_URL = "s3://psdj-landing";
+const AWS_S3_BACKED_URL = "s3://pcdj-landing";
 const STORAGE_FOLDER = AWS_S3_BACKED_URL . '/app_storage';
 const USER_REQUEST_FILE = "user_requests";
 const LOG_FILE = "log";
