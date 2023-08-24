1bac7bc4f5d3abd265e8d949d63ad4ace83a0f9c
Denis progman <95615037+denis-progman@users.noreply.github.com>
2023-08-24 00:05:44 -0400

    added shine animation to the donation button

--- index.php
+++ index.php
@@ -160,7 +160,9 @@
         <input type="hidden" name="no_recurring" value="0" />
         <input type="hidden" name="item_name" value="We are starting an unique EDM to help musicians promote the tracks, communicate between each other and find their listeners. " />
         <input type="hidden" name="currency_code" value="USD" />
-        <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+        <div class="image-wrapper shine">
+            <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+        </div>
         <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
     </form>
     <h6>⏏</h6>
